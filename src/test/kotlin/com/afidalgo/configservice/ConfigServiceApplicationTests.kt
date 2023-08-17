b28af44cfefa6bf3f2de40363fe9a27e1a229c50
afidalgoSE <afidalgose@gmail.com>
2023-08-16 22:13:49 -0400

    added github workflow and unit test

--- src/test/kotlin/com/afidalgo/configservice/ConfigServiceApplicationTests.kt
+++ src/test/kotlin/com/afidalgo/configservice/ConfigServiceApplicationTests.kt
@@ -1,13 +1,26 @@
 package com.afidalgo.configservice
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.cloud.config.environment.Environment
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.client.RestTemplate
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ConfigServiceApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+  val restTemplate = RestTemplate()
 
+  @Test fun contextLoads() {}
+
+  @Test
+  @Disabled("You can enable this test if your config-repo is publicly available")
+  fun defaultConfigurationAvailable() {
+    val responseEntity: ResponseEntity<Environment> =
+        restTemplate.getForEntity("/application/default", Environment::class.java)
+    assertThat(responseEntity.statusCode).isEqualTo(HttpStatus.OK)
+  }
 }
